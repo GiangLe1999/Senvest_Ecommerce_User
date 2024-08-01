3f1552273f7de9fd1ca100e9d8f39d0c61f6f9d1
Lê Thanh Giang <100833182+GiangLe1999@users.noreply.github.com>
2024-08-01 23:00:15 +0700

    Fix next-intl bug:

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,9 +1,15 @@
-import { useTranslations } from "next-intl";
+import { NextPage } from "next";
+import { unstable_setRequestLocale } from "next-intl/server";
 
-export default function Home() {
-  const t = useTranslations("common");
+interface Props {
+  params: {
+    locale: string;
+  };
+}
 
+const HomePage: NextPage<Props> = ({ params: { locale } }: Props) => {
+  unstable_setRequestLocale(locale);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
   );
-}
+};
