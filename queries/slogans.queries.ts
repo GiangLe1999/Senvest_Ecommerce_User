af7e976aaa29e946cc9b59118cdcbc3e2af5e52c
Lê Thanh Giang <100833182+GiangLe1999@users.noreply.github.com>
2024-08-02 11:50:15 +0700

    Add revalidate for fetch slogans

--- queries/slogans.queries.ts
+++ queries/slogans.queries.ts
@@ -2,14 +2,13 @@
 import { publicAxiosInstance } from "@/configs/axios";
 
 export const getSlogans = async () => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/slogans`);
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/slogans`, {
+    next: { revalidate: 10 },
+  });
 
   if (!res.ok) {
     throw new Error("Failed to fetch data");
   }
 
   return await res.json();
-  // const data = await publicAxiosInstance.get("/slogans");
-
-  // return data;
 };
