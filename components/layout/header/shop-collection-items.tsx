70be2d2e87e7f359dce9eb8952a1d026f3c60e25
Lê Thanh Giang <100833182+GiangLe1999@users.noreply.github.com>
2024-08-03 14:44:33 +0700

    Update ico

--- components/layout/header/shop-collection-items.tsx
+++ components/layout/header/shop-collection-items.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Skeleton } from "@/components/ui/skeleton";
 import { Link } from "@/configs/i18n-navigation";
 import { Category } from "@/entities/category.entity";
 import { getCategoriesForNavigation } from "@/queries/categories.queries";
@@ -21,17 +22,17 @@ const ShopCollectionItems: FC<Props> = (props): JSX.Element => {
   const locale = useLocale();
 
   return (
-    <nav className="border shadow-md p-5 absolute top-full left-0 w-full grid grid-cols-4 rounded-sm">
+    <nav className="border shadow-md p-5 absolute top-full left-0 w-full grid grid-cols-4 gap-4 rounded-sm">
       <div>
         <p className="font-bold">{t("featured")}</p>
         <ul className="text-muted text-[13px] space-y-1 mt-2">
-          <li className="py-1 hover:text-primary transition-colors">
+          <li className="py-1 my-1 hover:text-primary transition-colors">
             <Link href="/bo-suu-tap/san-pham-moi">{t("new_arrivals")}</Link>
           </li>
-          <li className="py-1 hover:text-primary transition-colors">
+          <li className="py-1 my-1 hover:text-primary transition-colors">
             <Link href="/bo-suu-tap/ban-chay">{t("best_sellers")}</Link>
           </li>
-          <li className="py-1 hover:text-primary transition-colors">
+          <li className="py-1 my-1 hover:text-primary transition-colors">
             <Link href="/bo-suu-tap/ban-chay">{t("the_sale_room")}</Link>
           </li>
         </ul>
@@ -39,20 +40,27 @@ const ShopCollectionItems: FC<Props> = (props): JSX.Element => {
 
       <div>
         <p className="font-bold">{t("categories")}</p>
-        <ul className="text-muted text-[13px] space-y-1 mt-2">
-          {isPending
-            ? "loading"
-            : data?.categories?.map((category) => (
-                <li
-                  key={category._id}
-                  className="py-1 hover:text-primary transition-colors capitalize"
-                >
-                  <Link href={`/bo-suu-tap/${category.slug[locale]}` as any}>
-                    {category.name[locale]}
-                  </Link>
-                </li>
-              ))}
-        </ul>
+
+        {isPending ? (
+          <div className="space-y-3 mt-2">
+            {Array.from(Array(3).keys()).map((key) => (
+              <Skeleton className="h-6 w-full" key={key} />
+            ))}
+          </div>
+        ) : (
+          <ul className="text-muted text-[13px] mt-2">
+            {data?.categories?.map((category) => (
+              <li
+                key={category._id}
+                className="py-1 my-1 hover:text-primary transition-colors capitalize"
+              >
+                <Link href={`/bo-suu-tap/${category.slug[locale]}` as any}>
+                  {category.name[locale]}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
 
       <div>
