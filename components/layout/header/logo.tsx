631df0ec2f702e3f3f1b80f92557002224f06c0c
Lê Thanh Giang <100833182+GiangLe1999@users.noreply.github.com>
2024-09-14 23:13:35 +0700

    Update logo

--- components/layout/header/logo.tsx
+++ components/layout/header/logo.tsx
@@ -8,7 +8,7 @@ const Logo: FC<Props> = (props): JSX.Element => {
   return (
     <Link href="/" className="flex items-center h-full">
       <Image
-        src="/logo.svg"
+        src="/logo.png"
         alt="Kindle Hope Candles Logo"
         width={75}
         height={56.25}
